9c09e1cd397cf6d19b7e2e14e02b7f7083c5dfb8
Parinith Gowda G <121385246+P4R1N17Hmain@users.noreply.github.com>
2025-02-10 15:33:52 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+import type { Configuration } from "webpack";
+
+const nextConfig: NextConfig = {
+  webpack(config: Configuration) {
+    config.module?.rules?.push({
+      test: /\.(mp4|webm)$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name].[hash][ext]",
+      },
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      borderWidth: {
+        border: "1px", // Define `border-border` with a custom value
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
